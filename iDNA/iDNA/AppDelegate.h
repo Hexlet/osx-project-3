2d3ce8eeb7ee0e2a7ceefcc2f71654cd061bc236
Alex Ovseyenko <mixtline@gmail.com>
2012-12-17 21:43:27 +0400

    initial interface

---
+++ iDNA/iDNA/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iDNA
+//
+//  Created by alex on 17/12/2012.
+//  Copyright (c) 2012 alex. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ iDNA/iDNA/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  iDNA
+//
+//  Created by alex on 17/12/2012.
+//  Copyright (c) 2012 alex. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
