589dcf832978642c50c69896d2aab005d66629c5
ShSergey <shsergeyn@mail.ru>
2012-12-23 16:30:58 +0400

    предварительный 1

--- DNA-project-3/DNA-project-3/Cell.m
+++ DNA-project-3/DNA-project-3/Cell.m
@@ -1,13 +1,106 @@
 //
 //  Cell.m
-//  DNA-project-3
+//  Project1DNK
 //
-//  Created by Sergey on 22.12.12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Created by Sergey on 02.11.12.
+//  Copyright (c) 2012 Sergey. All rights reserved.
 //
 
 #import "Cell.h"
 
 @implementation Cell
+
+@synthesize hammingDistance;
+@synthesize DNA = _DNA;
+
+-(id) init {
+    self = [super init];
+    if (self) {
+        arrayATGC = [[NSArray alloc] initWithObjects:@"A", @"T", @"G", @"C",nil];
+        _DNA = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+
+-(void)initWithDNALenght:(NSInteger) d {
+    dnaLenght = d;
+    for (int i=0; i<dnaLenght; i++) {
+        [_DNA addObject:[arrayATGC objectAtIndex: arc4random() % 4]];
+    }
+}
+
+-(int) calculateHammingDistance:(Cell*) c {
+    int counthammingDistance = 0;
+    for ( int i=0; i<100; i++) {
+        if ([[_DNA objectAtIndex:i] isEqualToString: [c.DNA objectAtIndex: i]]) {
+           counthammingDistance ++;
+        }
+    }
+    hammingDistance = counthammingDistance;
+    return counthammingDistance;
+}
+
+-(void)mutate:(NSInteger) percent {
+    NSMutableArray* mutateCell = [[NSMutableArray alloc] init];
+    NSNumber* num = [NSNumber alloc];
+    int x = percent*dnaLenght/100;
+    int num_int, num_int2;
+    for (int i=1; i<=x; i++) {
+        num_int = arc4random() % dnaLenght;
+        while ([mutateCell containsObject:[num initWithInt:num_int]]==YES) {
+            //           NSLog(@"уже менялась ячейка %i",num_int);
+            num_int = arc4random() % dnaLenght;
+        }
+        [mutateCell addObject:[num initWithInt:num_int]];
+        //       NSLog(@"меняем %i раз, ячейку %i",i,num_int);
+        num_int2 = arc4random() % 4;
+        while ([[self.DNA objectAtIndex:num_int] isEqualToString:[arrayATGC objectAtIndex:num_int2]] == YES)  {
+            //       NSLog(@"совпало в ячейке %i",num_int2);
+            num_int2 = arc4random() % 4;
+        }
+        [self.DNA replaceObjectAtIndex:num_int withObject:[arrayATGC objectAtIndex:num_int2]];
+    }
+}
+
+-(NSString*)stringDNA {
+    NSString *outString = @"";
+    for(int i=0; i<dnaLenght; i++)
+        outString = [outString stringByAppendingString:[_DNA objectAtIndex:i]];
+    return outString;
+}
+
+-(Cell*)crossing:(Cell*) c {
+    Cell* crossingCell;
+    crossingCell = [[Cell alloc] init];
+    int num_variant = arc4random() % 3;
+    if (num_variant == 1) {
+        int halfdnaLenght = dnaLenght/2; //50%
+        for(int i=0; i<dnaLenght; i++) {
+            if ((i+1)<halfdnaLenght)
+                [crossingCell.DNA addObject:[_DNA objectAtIndex:i]];
+            else
+                [crossingCell.DNA addObject:[c.DNA objectAtIndex:i]];
+        }
+     }
+     else if (num_variant == 2) {
+         for(int i=0; i<dnaLenght; i++) {
+             if ((i%2) == 0)
+                 [crossingCell.DNA addObject:[_DNA objectAtIndex:i]];
+             else
+                 [crossingCell.DNA addObject:[c.DNA objectAtIndex:i]];
+         }
+     }
+     else {
+        int halfdnaLenght = dnaLenght/5; //20%
+        for(int i=0; i<dnaLenght; i++) {
+            if ((i+1)<halfdnaLenght || (i+1)>(dnaLenght-halfdnaLenght))
+                [crossingCell.DNA addObject:[_DNA objectAtIndex:i]];
+            else
+                [crossingCell.DNA addObject:[c.DNA objectAtIndex:i]];
+        }
+     }
+    return  crossingCell;
+}
+
 
 @end
