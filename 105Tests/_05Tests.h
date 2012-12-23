fa4bb2df9fd2c1a2b0fde1af3223450aeeae6339
khudenkon <khudenkon@yandex.ru>
2012-12-23 12:07:39 +0200

    osx-project-1

---
+++ 105Tests/_05Tests.h
@@ -0,0 +1,13 @@
+//
+//  _05Tests.h
+//  105Tests
+//
+//  Created by Stas on 12/15/12.
+//  Copyright (c) 2012 Stas. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface _05Tests : SenTestCase
+
+@end
---
+++ 105Tests/_05Tests.m
@@ -0,0 +1,32 @@
+//
+//  _05Tests.m
+//  105Tests
+//
+//  Created by Stas on 12/15/12.
+//  Copyright (c) 2012 Stas. All rights reserved.
+//
+
+#import "_05Tests.h"
+
+@implementation _05Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in 105Tests");
+}
+
+@end
