564b745138e1904e5db6a85463d9f875ff153edd
fedoroff <administrator@Administrators-Mac.local>
2012-12-25 17:22:16 +0200

    2

--- Just Another Simple Renamer/Just Another Simple Renamer/AppDelegate.h
+++ Just Another Simple Renamer/Just Another Simple Renamer/AppDelegate.h
@@ -8,8 +8,21 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface AppDelegate : NSObject <NSApplicationDelegate>
+@interface AppDelegate : NSObject <NSApplicationDelegate>{
+    NSMutableArray *ResultFileNames, *SourceFilesArray;
+}
 
 @property (assign) IBOutlet NSWindow *window;
+
+@property (weak) IBOutlet NSTextField *PrefixName;
+@property (weak) IBOutlet NSTextField *CounterMask;
+@property (weak) IBOutlet NSTextField *CounterStartFrom;
+@property (weak) IBOutlet NSTableView *SourceTableView;
+@property (weak) IBOutlet NSTableView *ResultTableView;
+
+- (IBAction)addFiles:(id)sender;
+- (IBAction)addFolder:(id)sender;
+- (IBAction)clearList:(id)sender;
+- (IBAction)goRename:(id)sender;
 
 @end
--- Just Another Simple Renamer/Just Another Simple Renamer/AppDelegate.m
+++ Just Another Simple Renamer/Just Another Simple Renamer/AppDelegate.m
@@ -10,9 +10,68 @@
 
 @implementation AppDelegate
 
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+    ResultFileNames = [NSMutableArray array];
+    SourceFilesArray = [NSMutableArray array];
     // Insert code here to initialize your application
+}
+
+- (IBAction)addFiles:(id)sender {
+    NSOpenPanel* openDialog = [NSOpenPanel openPanel];
+    [openDialog setAllowsMultipleSelection:YES];
+    if ([openDialog runModal] == NSOKButton) {
+        [SourceFilesArray addObjectsFromArray:[openDialog URLs]];
+        NSLog(@"%@", SourceFilesArray);
+    }
+    [_SourceTableView reloadData];
+}
+
+- (IBAction)addFolder:(id)sender {
+    NSOpenPanel* openDialog = [NSOpenPanel openPanel];
+    [openDialog setCanChooseFiles:NO];
+    [openDialog setCanChooseDirectories:YES];
+    if ([openDialog runModal] == NSOKButton) {
+        [SourceFilesArray addObjectsFromArray:[[NSFileManager defaultManager] contentsOfDirectoryAtURL:[[openDialog URLs]objectAtIndex:0] includingPropertiesForKeys:nil options:Nil error:nil]];
+        NSLog(@"%@", SourceFilesArray);
+    }
+    [_SourceTableView reloadData];
+}
+
+- (IBAction)clearList:(id)sender {
+    [SourceFilesArray removeAllObjects];
+    [_SourceTableView reloadData];
+}
+
+- (IBAction)goRename:(id)sender {
+}
+
+-(NSInteger)numberOfRowsInTableView:(NSTableView*) SomeTableView {
+    if (SomeTableView == _SourceTableView) {
+        return [SourceFilesArray count];
+    }
+    else if (SomeTableView == _ResultTableView) {
+        return [ResultFileNames count];
+    }
+    else {
+        NSLog(@"numberOfRowsInTableView returning 0");
+        return 0;
+    }
+}
+
+-(id)tableView:(NSTableView*) SomeTableView objectValueForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
+    if (SomeTableView == _SourceTableView) {
+        NSString *v = [SourceFilesArray objectAtIndex:row];
+        return v;
+    }
+    else if (SomeTableView == _ResultTableView) {
+        return [ResultFileNames objectAtIndex:row];
+    }
+    else {
+        NSLog(@"objectValueForTableColumn returning 0");
+        return 0;
+    }
 }
 
 @end
