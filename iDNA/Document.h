ea25ec4063d32640c1cebbcfd3101a97eea16888
Kirill Gorshkov <kan.kiryan@gmail.com>
2012-12-25 12:02:25 +0400

    [ADD] project tree

---
+++ iDNA/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  iDNA
+//
+//  Created by Kirill Gorshkov on 24.12.12.
+//  Copyright (c) 2012 Kirill Gorshkov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
---
+++ iDNA/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  iDNA
+//
+//  Created by Kirill Gorshkov on 24.12.12.
+//  Copyright (c) 2012 Kirill Gorshkov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
