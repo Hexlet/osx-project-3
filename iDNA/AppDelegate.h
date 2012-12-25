43b837a55a531d95c6eb12dad9b20b06955cfeb2
Roman Evseev <roman.evseev@mail.ru>
2012-12-25 23:53:52 +0400

    Initial commit

---
+++ iDNA/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iDNA
+//
+//  Created by Роман Евсеев on 25.12.12.
+//  Copyright (c) 2012 Роман Евсеев. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ iDNA/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  iDNA
+//
+//  Created by Роман Евсеев on 25.12.12.
+//  Copyright (c) 2012 Роман Евсеев. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
