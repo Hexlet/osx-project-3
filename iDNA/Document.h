5502a1481f89f10dfd714b759e068e89831a708f
Mac User <macuser@Mac-Users-iMac.local>
2012-12-25 15:41:17 +0200

    Initial Commit

---
+++ iDNA/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  iDNA
+//
+//  Created by Mac User on 12/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
