dc9c7a4b00a04ffdc8d468968d1a2e9b0ddba9b4
Vladislav Khazov <hadush@live.ru>
2012-12-25 21:40:05 +0400

    iDNA by Khazov
    Добавил Load Goal DNA

--- iDNA/iDNA/HVSDocument.m
+++ iDNA/iDNA/HVSDocument.m
@@ -51,23 +51,29 @@
 //Действия
 -(void)startBackgroundEvolution {
     //Эволюция
+    //Выполняем пока не получим 100: совпадений или пока не нажмут на кнопку Пауза
     while ([myPopulation flag]==NO && flagPause==NO) {
+        [myPopulation evolution];
         [_popLevelMatch setIntegerValue:myPopulation.maxHamming];
         [_popLabelMatch setIntegerValue:myPopulation.maxHamming];
         [_popLabelGeneration setIntegerValue:myPopulation.countEvolution];
-        [myPopulation evolution];
     }
-    //Меняем интерфейс
-    [_popTextSize setEnabled:YES];
-    [_popTextLength setEnabled:YES];
-    [_popTextRate setEnabled:YES];
     
-    [_popSliderSize setEnabled:YES];
-    [_popSliderLength setEnabled:YES];
+    //Меняем интерфейс если не Пауза
+    if (flagPause==NO) {
+      [_popTextSize setEnabled:YES];
+      [_popTextLength setEnabled:YES];
+    
+      [_popSliderSize setEnabled:YES];
+      [_popSliderLength setEnabled:YES];
+    
+      [_popButtonLoad setEnabled:YES];
+    }
+    
+    //Разрешаем менять % мутации, даже на паузе.
     [_popSliderRate setEnabled:YES];
-    
+    [_popTextRate setEnabled:YES];
     [_popButtonStart setEnabled:YES];
-    [_popButtonLoad setEnabled:YES];
     [_popButtonPause setEnabled:NO];
 }
 
@@ -75,6 +81,7 @@
 //Нажата кнопка Старт
 - (IBAction)buttonStart:(id)sender {
     //Меняем интерфейс
+    
     [_popTextSize setEnabled:NO];
     [_popTextLength setEnabled:NO];
     [_popTextRate setEnabled:NO];
@@ -86,16 +93,18 @@
     [_popButtonStart setEnabled:NO];
     [_popButtonLoad setEnabled:NO];
     [_popButtonPause setEnabled:YES];
-    
-    //Создаем случайную популяцию ДНК с заданными параметрами.
-    [myPopulation setPopulation];
-    // Устанавливаем флаг совпадений в NO
-    [myPopulation setFlag:NO];
-    //Совпадение с Альфа
-    [myPopulation setMaxHamming:0];
-    //Количество эволюций
-    [myPopulation setCountEvolution:0];
-    
+
+    //Если это не после паузы
+    if (flagPause==NO) {
+      //Создаем случайную популяцию ДНК с заданными параметрами.
+      [myPopulation setPopulation];
+      // Устанавливаем флаг совпадений в NO
+      [myPopulation setFlag:NO];
+      //Совпадение с Альфа
+      [myPopulation setMaxHamming:0];
+      //Количество эволюций
+      [myPopulation setCountEvolution:0];
+    }
     //Эволюция - запускаем фоном
     flagPause=NO;
     [self performSelectorInBackground:@selector(startBackgroundEvolution) withObject:nil];
@@ -105,6 +114,55 @@
     
 }
 - (IBAction)buttonLoad:(id)sender {
+    //Загрузка файла текстового поля
+
+    //Создаем File Open Dialog class
+    NSOpenPanel* openDlg = [NSOpenPanel openPanel];
+    
+    //Включаем выбор файлов
+    [openDlg setCanChooseFiles:YES];
+    //отключаем выбор папок
+    [openDlg setCanChooseDirectories:NO];
+//    [openDlg setRequireFileType:@"txt"];
+    //разрешаем только расширение txt
+    [openDlg setAllowedFileTypes:[[NSArray alloc]initWithObjects:@"txt", nil]];
+    //Остальные разрешенеия запрещаем выбирать
+    [openDlg setAllowsOtherFileTypes:NO];
+    
+    // Запускаем и ждем  Окей
+    if ( [openDlg runModal] == NSOKButton )
+    {
+        // Получаем список выбранных файлов
+        NSArray* files = [openDlg URLs] ;
+        
+        BOOL symbol=NO;
+        // Проходим по ним
+        for( int i = 0; i < [files count]; i++ )
+        {
+            //Получаем Path первого выбранного
+            NSString* fileName = [[files objectAtIndex:i] path];
+            //Считываем все подряд
+            NSString * fileContents = [NSString stringWithContentsOfFile:fileName encoding:NSUTF8StringEncoding error:nil];
+            // Записываем его есои он устраивает нашим параметрам
+            if ([fileContents length]==[myPopulation populationLengthDna] && [fileContents length]<=100) {
+                //Устанавливаем текстовое поле
+                [_popTextGoalDna setStringValue:fileContents];
+                if (symbol==NO) {
+                    //создаем нового альфа самца размерностью файла
+                    [[myPopulation goalDNA] setDNA:[[NSMutableArray alloc]initWithContentsOfFile:fileContents]];
+                } else {
+                    NSAlert *myAlert = [NSAlert alertWithMessageText:@"Похоже файл содержит символ не относящийся к ДНК!!!" defaultButton:@"Okey" alternateButton:nil otherButton:nil informativeTextWithFormat:@" "];
+                     [myAlert runModal];
+                }
+                
+            } else {
+                NSString *str=[[NSMutableString alloc]initWithFormat:@"Данный файл имеет длину %ld",[fileContents length]];
+                NSAlert *myAlert = [NSAlert alertWithMessageText:@"Файл содердит ДНК другой размерностью или его размер больше 100 единиц!!! " defaultButton:@"Okey" alternateButton:nil otherButton:nil informativeTextWithFormat:str];
+                [myAlert runModal];
+            }
+        
+        }
+    }
     
 }
 
