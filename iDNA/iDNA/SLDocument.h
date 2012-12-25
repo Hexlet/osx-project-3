53d1bd49eb7db2e7dc345dfa97a437b14fe30bd8
slebedev <sergey.al.lebedev@gmail.com>
2012-12-25 21:06:25 +0400

    initial commit by me

---
+++ iDNA/iDNA/SLDocument.h
@@ -0,0 +1,13 @@
+//
+//  SLDocument.h
+//  iDNA
+//
+//  Created by S.Lebedev on 25.12.12.
+//  Copyright (c) 2012 S.Lebedev. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SLDocument : NSDocument
+
+@end
---
+++ iDNA/iDNA/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  iDNA
+//
+//  Created by S.Lebedev on 25.12.12.
+//  Copyright (c) 2012 S.Lebedev. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
