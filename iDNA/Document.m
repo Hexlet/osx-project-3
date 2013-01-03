1cece6dd952e83175cfdc8d2b4bef28540e27432
khudenkon <khudenkon@yandex.ru>
2013-01-03 11:12:38 +0200

    FINAL

--- iDNA/Document.m
+++ iDNA/Document.m
@@ -153,7 +153,7 @@ ResultController *resultWindow;
              NSLog(@"After sorting:");
              PRINT_DNAS
              */
-
+            
             [lbgeneration setIntegerValue:countOfGeneration];
         
             // проверка: создана ли особь, совпадающая с Goal DNA
