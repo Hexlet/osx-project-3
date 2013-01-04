984daf945d68a62e68910f8657d1d026fec50448
fedoroff <fedoroff.andrew@gmail.com>
2013-01-04 18:47:17 +0200

    see readme

--- Just Another Simple Renamer/Just Another Simple Renamer/AppDelegate.m
+++ Just Another Simple Renamer/Just Another Simple Renamer/AppDelegate.m
@@ -23,18 +23,31 @@
     [openDialog setAllowsMultipleSelection:YES];
     if ([openDialog runModal] == NSOKButton) {
         [SourceFilesArray addObjectsFromArray:[openDialog URLs]];
-        NSLog(@"%@", SourceFilesArray);
     }
     [_SourceTableView reloadData];
 }
 
 - (IBAction)addFolder:(id)sender {
     NSOpenPanel* openDialog = [NSOpenPanel openPanel];
+    NSError *error = nil;
+    NSArray *URLList;
     [openDialog setCanChooseFiles:NO];
     [openDialog setCanChooseDirectories:YES];
     if ([openDialog runModal] == NSOKButton) {
-        [SourceFilesArray addObjectsFromArray:[[NSFileManager defaultManager] contentsOfDirectoryAtURL:[[openDialog URLs]objectAtIndex:0] includingPropertiesForKeys:nil options:Nil error:nil]];
-        NSLog(@"%@", SourceFilesArray);
+        //Получаем список URL'ов
+        URLList = [[NSFileManager defaultManager]
+          contentsOfDirectoryAtURL:[[openDialog URLs]objectAtIndex:0]
+          includingPropertiesForKeys:[NSArray array]
+          options:(NSDirectoryEnumerationSkipsHiddenFiles)
+          error:&error];
+        //Добавляем только файлы, папки не берем
+        for (NSURL *fileURL in URLList) {
+            NSNumber *isDirectory = nil;
+            [fileURL getResourceValue:&isDirectory forKey:NSURLIsDirectoryKey error:nil];
+            if (![isDirectory boolValue]) {
+                [SourceFilesArray addObject:fileURL];
+            }
+        }
     }
     [_SourceTableView reloadData];
 }
@@ -45,6 +58,16 @@
 }
 
 - (IBAction)goRename:(id)sender {
+    if ([SourceFilesArray count] >0) {
+        [ResultFileNames removeAllObjects];     //Очищаем массив каждый раз, т.к. таблица целевых имен должна динамически реагировать на изменения в полях ввода
+        for (NSInteger i = [_CounterStartFrom integerValue]; [ResultFileNames count] < [SourceFilesArray count]; i++) {
+            NSString *str = [NSString stringWithString:[_PrefixName stringValue]];
+            str = [str stringByAppendingString:[NSString stringWithFormat:[_CounterMask stringValue], i]];
+            [ResultFileNames addObject:str];
+        }
+    }
+    
+    [_ResultTableView reloadData];
 }
 
 -(NSInteger)numberOfRowsInTableView:(NSTableView*) SomeTableView {
@@ -62,7 +85,7 @@
 
 -(id)tableView:(NSTableView*) SomeTableView objectValueForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
     if (SomeTableView == _SourceTableView) {
-        NSString *v = [SourceFilesArray objectAtIndex:row];
+        NSString *v = [[[SourceFilesArray objectAtIndex:row] absoluteString] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
         return v;
     }
     else if (SomeTableView == _ResultTableView) {
