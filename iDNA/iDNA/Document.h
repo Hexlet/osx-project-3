29bf8928c61e857ae8126e920ae012d0d6d7cfb5
vladimir <vladimir@MacBook-Air-Vladimir.local>
2013-01-14 23:36:01 +0400

    START

---
+++ iDNA/iDNA/Document.h
@@ -0,0 +1,14 @@
+//
+//  Document.h
+//  iDNA
+//
+//  Created by vladimir on 14.01.13.
+//  Copyright (c) 2013 Владимир Ковалев. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "Cell+mutate.h"
+
+@interface Document : NSDocument
+
+@end
---
+++ iDNA/iDNA/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  iDNA
+//
+//  Created by vladimir on 14.01.13.
+//  Copyright (c) 2013 Владимир Ковалев. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
